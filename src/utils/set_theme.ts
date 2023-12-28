5de3999c7a9a48602b376952f7ac6d65e41cacc0
CRT-123 <1130894061@qq.com>
2023-12-28 18:32:48 +0800

    1

--- src/utils/set_theme.ts
+++ src/utils/set_theme.ts
@@ -1,6 +1,7 @@
 // 引入pinia
 import useStore from '@/store'
 import { storeToRefs } from "pinia";
+import { ref } from 'vue';
 export function saveThemePreference() {
     const { home } = useStore()
     home.setTheme()
@@ -9,6 +10,7 @@ export function saveThemePreference() {
 export function loadThemePreference() {
     const { home } = useStore()
     const { theme } = storeToRefs(home)
+    isDarkMode.value = true;
     if (theme.value) {
         document.body.classList.add('dark-theme');
     }
@@ -28,3 +30,30 @@ export function toggleTheme() {
 
 
 
+import { computed, watch } from "vue";
+
+
+// 创建一个响应式变量
+const isDarkMode = ref(false);
+
+
+
+
+// 页面加载时加载主题偏好
+document.addEventListener('DOMContentLoaded', () => {
+    loadThemePreference();
+
+});
+
+
+
+
+// 使用 MutationObserver 监听 body 类的变化
+const observer = new MutationObserver(() => {
+    isDarkMode.value = document.body.classList.contains('dark-theme');
+});
+observer.observe(document.body, { attributes: true, attributeFilter: ['class'] });
+
+
+
+export default isDarkMode
